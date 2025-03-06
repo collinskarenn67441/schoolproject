2591dbfb92f9de8c87265882704b742c4c052576
collinskarenn67441 <collins_karenn67441@gmx.com>
2025-03-06 12:09:48 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,9 @@
+int main() {
+    int x = 5;
+    if(x > 10) {
+        std::cout << "x is greater than 10" << std::endl;
+    } else {
+        std::cout << "x is not greater than 10" << std::endl;
+    }
+    return 0;
+}
